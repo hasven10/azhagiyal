f53b4d9fc4a3ae2a6f45e794388dbf8eb4582469
Hasventhran Baskaran <hasventhran@gmail.com>
2026-03-10 14:35:48 +0800

    modify some pages

--- src/routes/fashion/[subcategory]/+page.js
+++ src/routes/fashion/[subcategory]/+page.js
@@ -3,3 +3,17 @@ export function load({ params }) {
     subcategory: params.subcategory
   };
 }
+
+export function entries() {
+  return [
+    { subcategory: 'dresses' },
+    { subcategory: 'tops' },
+    { subcategory: 'kurtas' },
+    { subcategory: 'sarees' },
+    { subcategory: 'bottomwear' },
+    { subcategory: 'coord-sets' },
+    { subcategory: 'jewelleries' },
+    { subcategory: 'bags' },
+    { subcategory: 'earrings' }
+  ];
+}
